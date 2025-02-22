63b0faf4a04a3bd7260dc8b327a5290eab00d008
diego <42916420+dacampos1@users.noreply.github.com>
2025-02-22 13:43:30 -0300

    feat: Random payloads

--- 2/src/simulator/handler.ts
+++ 2/src/simulator/handler.ts
@@ -32,8 +32,10 @@ const getRandomPayloads = (currentIdNumber: number) => {
   const NAMES = ['Leia', "Sadie", "Jose", "Sara", "Frank", "Dewey", "Tomas", "Joel", "Lukas", "Carlos"];
   const LAST_NAMES = ["Liberty", "Ray", "Harrison", "Ronan", "Drew", "Powell", "Larsen", "Chan", "Anderson"];
   const randomNumberOfParallelRequests = Math.floor(Math.random() * 5) + 2;
+  
   const payloads = Array.from({ length: randomNumberOfParallelRequests }, (_, indexPayload) => {
-    const customers = Array.from({ length: 2 }, (_, indexCustomer) => ({
+    const randomNumberOfCustomers= Math.floor(Math.random() * 5) + 2;
+    const customers = Array.from({ length: randomNumberOfCustomers }, (_, indexCustomer) => ({
       firstName: NAMES[Math.floor(Math.random() * NAMES.length)],
       lastName: LAST_NAMES[Math.floor(Math.random() * LAST_NAMES.length)],
       age: Math.floor(Math.random() * 80) + 10,
