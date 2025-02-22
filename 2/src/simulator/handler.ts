e5ece042c064f0c669f9269861b01fcc5efd0356
diego <42916420+dacampos1@users.noreply.github.com>
2025-02-22 13:34:18 -0300

    feat: Add simulator

---
+++ 2/src/simulator/handler.ts
@@ -0,0 +1,44 @@
+import { APIGatewayProxyEventV2, APIGatewayProxyHandlerV2 } from 'aws-lambda';
+import axios from 'axios';
+
+
+
+let url = "http://localhost:3000/api";
+export const handler: APIGatewayProxyHandlerV2 = async (event: APIGatewayProxyEventV2) => {
+  try {
+    const BASE_INDEX = event.body ? JSON.parse(event.body).currentIdNumber : 0;
+    const payloads = getRandomPayloads(BASE_INDEX);
+    
+    const paralellRequests = payloads.map(async (payload) => {
+      const response = await axios.post(url, payload);
+      return response;
+    }
+  );
+  await Promise.allSettled(paralellRequests);
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: `Requests generated successfully`, payloads }),
+  };
+} catch (error) {
+  return {
+    statusCode: 500,
+    body: JSON.stringify({ message: `Internal Server Error: ${error}` }),
+  };
+}
+};
+
+
+const getRandomPayloads = (currentIdNumber: number) => {
+  const NAMES = ['Leia', "Sadie", "Jose", "Sara", "Frank", "Dewey", "Tomas", "Joel", "Lukas", "Carlos"];
+  const LAST_NAMES = ["Liberty", "Ray", "Harrison", "Ronan", "Drew", "Powell", "Larsen", "Chan", "Anderson"];
+  const randomNumberOfParallelRequests = Math.floor(Math.random() * 5) + 2;
+  const payloads = Array.from({ length: randomNumberOfParallelRequests }, (_, indexPayload) => {
+    const customers = Array.from({ length: 2 }, (_, indexCustomer) => ({
+      firstName: NAMES[Math.floor(Math.random() * NAMES.length)],
+      lastName: LAST_NAMES[Math.floor(Math.random() * LAST_NAMES.length)],
+      age: Math.floor(Math.random() * 80) + 10,
+      id: currentIdNumber + indexPayload * 2 + indexCustomer
+    })); return customers;
+  });
+  return payloads;
+};
